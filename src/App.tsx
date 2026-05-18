51e8dac8237364e8d44d551321829678c9ab6be7
Andrei Barbosa Lacerda <andreiwolds@outlook.com>
2026-05-18 12:02:50 -0300

    frist commit of this project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Fragment } from 'react/jsx-runtime'
+
+import "./styles/global.css"
+import "./styles/theme.css"
+import { Heading } from './assets/components/Heading'
+
+export function App(){
+    console.log("Olá Mundo!")
+    return(
+        <Fragment>
+            <Heading />
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Neque nam cum soluta voluptatum fugit magni blanditiis.
+                Voluptatem, at quam?
+                Quae aliquid voluptatum ullam et natus sed ipsa ipsam accusantium cum.
+            </p>
+        </Fragment>
+    )
+}
+    
---
+++ src/assets/components/Heading.tsx
@@ -0,0 +1,5 @@
+export function Heading(){
+ return <h1 className='heading'>  Olá Mundo!</h1>
+
+
+}
