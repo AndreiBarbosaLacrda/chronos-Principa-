bf4ea78f5caef888c0fd0cd877c1440f2cc5d829
Andrei Barbosa Lacerda <andreiwolds@outlook.com>
2026-05-26 10:00:18 -0300

    lazy class

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
 
 
+import { Container } from "./components/Container"
+import { Heading } from "./components/Heading"
 import "./styles/global.css"
 import "./styles/theme.css"
-import { Heading } from './components/Heading'
-import {TimerIcon } from 'lucide-react'
 
 export function App(){
     console.log("Ola mundo!")
     return(
         <>
-            <div className="container" >
-                <div className="content">
-              <Heading >  
-                Chronos
-                <button> 
-                    <TimerIcon/>    
-                </button>
-            </Heading>
-    
-            
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Neque nam cum soluta voluptatum fugit magni blanditiis.
-                Voluptatem, at quam?
-                Quae aliquid voluptatum ullam et natus sed ipsa ipsam accusantium cum.
-            </p>
+                <Container>
+                <section>
+                        <p>Logo</p>
+                    </section>
+                        
+                </Container>
+           
 
+                <Container>
+                <div className="content">
+                    <section>
+                        <p>Menu</p>
+                    </section>
                 </div>
-            </div >
+                </Container>
+
+                <Container>
+                <div className="content">
+                    <section>
+                        <p>Form</p>
+                    </section>
+                </div>
+                </Container>
+
+                <Container>
+                <div className="content">
+                    <section>
+                        <p>Footer</p>
+                    </section>
+                </div>
+                </Container>
         </>
     )
 }
